4d8e0766faf1738c253b0299530b6c5ea0df9598
GAIvanova20 <GAIvanova20@codingburgas.bg>
2022-06-25 15:24:12 +0300

    Add textures. Close #27

--- _build/Source.cpp
+++ _build/Source.cpp
@@ -2,15 +2,29 @@
 
 int main(void)
 {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    const int screenWidth = 960;
+    const int screenHeight = 894;
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    SetTargetFPS(60);
+
+    Texture2D mickey = LoadTexture("../resources/mickey.png");
+    float frameWidth = (float)(mickey.width / 3);
+
+    Texture2D map = LoadTexture("../resources/map.png");
+    float mapFrameWidth = (float)(map.width / 12);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        ClearBackground(BLACK);
+
+        DrawTextureRec(map, Rectangle{ 0, 0, mapFrameWidth, (float)map.height }, { 0, 15 }, WHITE);
+        DrawTextureRec(mickey, Rectangle{ 0, 0, frameWidth, (float)mickey.height }, { 20, 628 }, WHITE);
+
         EndDrawing();
     }
+    UnloadTexture(mickey);
+    UnloadTexture(map);
 
     CloseWindow();
 
